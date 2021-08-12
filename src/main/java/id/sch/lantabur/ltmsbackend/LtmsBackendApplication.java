ea066e16fb53e0d65466bfa2b010e629167dd177
Sayyid Yofa <sayyidyofa@gmail.com>
2021-08-12 22:19:22 +0700

    Init

---
+++ src/main/java/id/sch/lantabur/ltmsbackend/LtmsBackendApplication.java
@@ -0,0 +1,13 @@
+package id.sch.lantabur.ltmsbackend;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class LtmsBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LtmsBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/id/sch/lantabur/ltmsbackend/LtmsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package id.sch.lantabur.ltmsbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LtmsBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
