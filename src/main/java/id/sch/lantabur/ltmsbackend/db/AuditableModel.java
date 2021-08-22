386115bba799a4ba759890b2e1a227255997a65a
Sayyid Yofa <sayyidyofa@gmail.com>
2021-08-22 21:15:57 +0700

    Remove unused jpa event listener

--- src/main/java/id/sch/lantabur/ltmsbackend/db/AuditableModel.java
+++ src/main/java/id/sch/lantabur/ltmsbackend/db/AuditableModel.java
@@ -3,6 +3,5 @@ package id.sch.lantabur.ltmsbackend.db;
 import javax.persistence.MappedSuperclass;
 
 @MappedSuperclass
-//@EntityListeners(JpaEventListener.class)
 public abstract class AuditableModel extends BaseEntity {
 }
